5ddb3647239b167175c1e6dfe057ca03e0306cf0
maxwellbenton <maxwellbenton@gmail.com>
2018-11-02 13:09:58 -0400

    updated test to reflect new solution

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -6,59 +6,91 @@ require_relative '../lib/museum.rb'
 
 describe 'Museum' do
   describe '.new_from_csv' do
+
+    before(:example) do
+      @museum = Museum.new('Metropolitan Museum of Art')
+    end
+
     it 'exists' do
-      expect( Museum ).to respond_to(:new_from_csv)
+      expect( @museum ).to respond_to(:new_from_csv)
     end
-    
-    
+
+
     it 'creates galleries and stores them as class instances' do
-      Museum.new_from_csv
-      expect( Museum.galleries.length ).to eq(3)
-      expect( Museum.galleries.map {|gallery| gallery.name} ).to include("Red")
-      expect( Museum.galleries.map {|gallery| gallery.name} ).to include("Green")
-      expect( Museum.galleries.map {|gallery| gallery.name} ).to include("Blue")
+      @museum.new_from_csv('art_heist.csv')
+      expect( @museum.galleries.length ).to eq(3)
+      expect( @museum.galleries.map {|gallery| gallery.name} ).to include("Red")
+      expect( @museum.galleries.map {|gallery| gallery.name} ).to include("Green")
+      expect( @museum.galleries.map {|gallery| gallery.name} ).to include("Blue")
     end
-    
+
     # add more tests for artsits/paintings
   end
-  
+
   describe '.biggest_gallery' do
-    it 'exists' do
-      expect( Museum ).to respond_to(:biggest_gallery)
+    before(:example) do
+      @museum = Museum.new('Metropolitan Museum of Art')
+      @museum.new_from_csv('art_heist.csv')
     end
-    
+
+    it 'exists' do
+      expect( @museum ).to respond_to(:biggest_gallery)
+    end
+
     it 'returns the correct gallery' do
-      expect(Museum.biggest_gallery).to eq("Red")
+      expect(@museum.biggest_gallery).to eq("Red")
     end
   end
-  
+
   describe '.smallest_gallery' do
-    it 'exists' do
-      expect( Museum ).to respond_to(:smallest_gallery)
+
+    before(:example) do
+      @museum = Museum.new('Metropolitan Museum of Art')
+      @museum.new_from_csv('art_heist.csv')
     end
-    
+
+    it 'exists' do
+      expect( @museum ).to respond_to(:smallest_gallery)
+    end
+
     it 'returns the correct gallery' do
-      expect(Museum.smallest_gallery).to eq("Blue")
+      expect(@museum.smallest_gallery).to eq("Blue")
     end
   end
-  
+
   describe '.artist_most_occurring' do
-    it 'exists' do
-      expect( Museum ).to respond_to(:artist_most_occurring)
+
+    before(:example) do
+      @museum = Museum.new('Metropolitan Museum of Art')
+      @museum.new_from_csv('art_heist.csv')
     end
-    
+
+    it 'exists' do
+      expect( @museum ).to respond_to(:artist_most_occurring)
+    end
+
     it 'returns the correct artist' do
-      expect(Museum.artist_most_occurring).to eq("Pablo Picasso")
+      expect(@museum.artist_most_occurring).to eq("Pablo Picasso")
     end
   end
-  
+
   describe '.value_of_artist' do
-    it 'exists' do
-      expect( Museum ).to respond_to(:value_of_artist)
+
+    before(:example) do
+      @museum = Museum.new('Metropolitan Museum of Art')
+      @museum.new_from_csv('art_heist.csv')
     end
-    
+
+    it 'exists' do
+      expect( @museum ).to respond_to(:value_of_artist)
+    end
+
     it 'returns the correct value of Vincent van Goghs works' do
-      expect(Museum.value_of_artist("Vincent van Gogh")).to eq(194400000)
+      expect(@museum.value_of_artist("Vincent van Gogh")).to eq(194400000)
+    end
+
+    it 'returns the correct value of Mark Rothko works' do
+      expect(@museum.value_of_artist("Mark Rothko")).to eq(186000000)
     end
   end
 end
