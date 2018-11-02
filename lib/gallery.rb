2706e17717901d8d1bd3c804b967a94c83438383
maxwellbenton <maxwellbenton@gmail.com>
2018-11-02 13:31:06 -0400

    updated solution

---
+++ lib/gallery.rb
@@ -0,0 +1,38 @@
+class Gallery
+  attr_reader :name, :works
+
+  @@data = {}
+
+  def initialize(name)
+    @name = name
+    @works = []
+  end
+
+  def self.most_valuable
+    all.max_by do |k, g|
+      g.works.map(&:+)
+    end
+  end
+
+  def self.all
+    @@data.values
+  end
+
+  def self.find_or_create_by_name(gallery_name)
+    return @@data[gallery_name] if @@data.key?(gallery_name)
+    @@data[gallery_name] = Gallery.new(gallery_name)
+  end
+
+  def self.biggest
+    p @@data.values.map{|g| g.works.length}
+    @@data.values.max_by{ |g| g.works.length}.name
+  end
+
+  def self.smallest
+    @@data.values.min_by{ |g| g.works.length}.name
+  end
+
+  def <<(work)
+    @works << work
+  end
+end
--- lib/museum.rb
+++ lib/museum.rb
@@ -3,29 +3,41 @@ require 'csv'
 class Museum
 
   def initialize(name)
-
+    @name = name
   end
 
-  def new_from_csv(museum_csv_file)
+  def new_from_csv(museum_csv_filename)
     # Here's the data file, pass this to CSV.read (see the CSV documentation for
     # more help!
-    data_file = (File.join(File.dirname(__FILE__), '..', museum_csv_file))
+    data_file = (File.join(File.dirname(__FILE__), '..', museum_csv_filename))
+
+    CSV.foreach(data_file) do |row|
+      gallery = Gallery.find_or_create_by_name(row[1])
+      artist = Artist.find_or_create_by_name(row[3])
+      painting = Painting.create(row[0], row[2], artist, row[4])
+      artist.add_work(painting) if artist.works.none? {|existing_painting| existing_painting.title == painting.title }
+      gallery << painting
+    end
+  end
+
+  def galleries
+    Gallery.all
   end
 
   def biggest_gallery
-
+    Gallery.biggest
   end
 
   def smallest_gallery
-
+    Gallery.smallest
   end
 
   def artist_most_occurring
-
+    Artist.most_prolific.name
   end
 
   def value_of_artist(artist_name)
-
+    Artist.find_by_name(artist_name).portfolio_value
   end
 
 end
