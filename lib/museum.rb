e30337cfe8c4c25914cb09611bb1c096aa4b43e7
maxwellbenton <maxwellbenton@gmail.com>
2018-11-02 13:09:36 -0400

    updated solution

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,53 +1,47 @@
 class Museum
-  attr_accessor :id, :gallery, :artist, :value
-  
-  @@galleries = []
-  @@artists = []
-  
+  attr_accessor :galleries, :artists
 
-  def self.artists
-    @@artists
+  def initialize(name)
+    @artists = []
+    @galleries = []
   end
 
-  
-  def self.galleries
-    @@galleries
-  end
-
-  def self.new_from_csv
-    file = File.dirname(File.dirname(File.expand_path(__FILE__))) + '/art_heist.csv'
+  def new_from_csv(museum_csv_file)
+    file = File.dirname(File.dirname(File.expand_path(__FILE__))) + "/" + museum_csv_file
     paintings_file =  File.open(file).read
     rows = paintings_file.split("\n")
     rows.each do |row|
-      
+
       data = row.split(",")
       id = data[0]
-      artist =  @@artists.find {|artist| artist.name == data[3]} ? @@artists.find {|artist| artist.name == data[3]} : Artist.new(data[3])
-      gallery = @@galleries.find {|gallery| gallery.name == data[1]} ?
-        @@galleries.find {|gallery| gallery.name == data[1]}
-        : Gallery.new(data[1])
+
+      artist =  @artists.find {|artist| artist.name == data[3]} ? @artists.find {|artist| artist.name == data[3]} : Artist.new(data[3])
+
+      gallery = @galleries.find {|gallery| gallery.name == data[1]} ? @galleries.find {|gallery| gallery.name == data[1]} : Gallery.new(data[1])
+
       painting = Painting.new(data[2], artist, data[4].chomp.to_i)
+
       gallery.paintings << painting
       artist.paintings << painting
-    
-      @@artists << artist if @@artists.none? {|existing_artist| existing_artist == artist }
-      @@galleries << gallery if @@galleries.none? {|existing_gallery| existing_gallery == gallery }
+
+      @artists << artist if @artists.none? {|existing_artist| existing_artist == artist }
+      @galleries << gallery if @galleries.none? {|existing_gallery| existing_gallery == gallery }
     end
   end
 
-  def self.biggest_gallery
-    @@galleries.max_by {|gallery|
+  def biggest_gallery
+    @galleries.max_by {|gallery|
       gallery.paintings.length
     }.name
   end
 
-  def self.smallest_gallery
-    @@galleries.min_by {|gallery|
+  def smallest_gallery
+    @galleries.min_by {|gallery|
       gallery.paintings.length
     }.name
   end
 
-  def self.artist_most_occurring
+  def artist_most_occurring
     artist_hash = {}
     Painting.all.each {|painting|
       if artist_hash[painting.artist]
@@ -58,18 +52,12 @@ class Museum
     }
     artist_hash.max_by {|k,v| v}[0].name
   end
-  
-  def self.value_of_artist(artist_name)
-    artist = @@artists.find {|artist| artist.name == artist_name}
+
+  def value_of_artist(artist_name)
+    artist = @artists.find {|artist| artist.name == artist_name}
     values = artist.paintings.map {|painting| painting.value}
     values.inject(0) {|sum, x| sum + x}
   end
-  
-  def self.value_of_artist(artist_name)
-    artist = @@artists.find {|artist| artist.name == artist_name}
-    values = artist.paintings.map {|painting| painting.value}
-    values.inject(0) {|sum, x| sum + x}
-  end
-  
+
 
 end
