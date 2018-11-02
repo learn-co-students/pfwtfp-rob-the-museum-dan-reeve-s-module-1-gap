223d9c96987bbc5aa682b6b1af1ee57dd2c44548
maxwellbenton <maxwellbenton@gmail.com>
2018-11-02 13:31:06 -0400

    updated solution

---
+++ lib/artist.rb
@@ -0,0 +1,44 @@
+class Artist
+  attr_reader :name, :works
+
+  @@data = {}
+
+  def initialize(name)
+    @name = name
+    @works = []
+  end
+
+  def self.all
+    @@data
+  end
+
+  def self.find_or_create_by_name(name)
+    return @@data[name] if @@data.has_key?(name)
+    x = self.new(name)
+    @@data[name] = x
+  end
+
+  def self.most_valuable
+    @@data.max_by{ |k, a| a.portfolio_value }.last
+  end
+
+  def self.most_prolific
+    @@data.max_by{ |k,a| a.works.length }.last
+  end
+
+  def self.find_by_name(name)
+    @@data[name]
+  end
+
+  def to_s
+    @name
+  end
+
+  def add_work(w)
+    @works << w
+  end
+
+  def portfolio_value
+    @works.map(&:value).map(&:to_i).reduce(&:+)
+  end
+end
