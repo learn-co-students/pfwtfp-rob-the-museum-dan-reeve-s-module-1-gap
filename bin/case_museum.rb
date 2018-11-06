d258b7b5dc36e24885b84bff7feca3ea017f22ee
maxwellbenton <maxwellbenton@gmail.com>
2018-11-06 08:40:11 -0500

    updated museum call arguments

--- bin/case_museum.rb
+++ bin/case_museum.rb
@@ -3,5 +3,5 @@ require_relative '../lib/museum.rb'
 
 # Run 'ruby bin/case_museum.rb' from lesson directory to start pry session
 
-museum = Museum.new('Metropolitan Museum of Art')
+museum = Museum.new('Metropolitan Museum of Art', 'art_heist.csv')
 binding.pry
