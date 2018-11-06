596844650ac7c25d8db2c5d19c1d643422976f1e
maxwellbenton <maxwellbenton@gmail.com>
2018-11-06 08:40:18 -0500

    updated starter code

--- lib/museum.rb
+++ lib/museum.rb
@@ -2,14 +2,15 @@ require 'csv'
 
 class Museum
 
-  def initialize(name)
-
+  def initialize(name, museum_csv_filename)
+    @name = name
+    @museum_csv_filename = museum_csv_filename
   end
 
-  def new_from_csv(museum_csv_file)
+  def new_from_csv
     # Here's the data file, pass this to CSV.read (see the CSV documentation for
     # more help!
-    data_file = (File.join(File.dirname(__FILE__), '..', museum_csv_file))
+    data_file = (File.join(File.dirname(__FILE__), '..', @museum_csv_filename))
   end
 
   def biggest_gallery
