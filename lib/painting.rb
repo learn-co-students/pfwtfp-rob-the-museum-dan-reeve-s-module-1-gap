1125bdacf433fbccedbebc623ad2a806c1dd7e59
Dan Reeves <you@example.com>
2019-09-28 02:05:38 -0400

    Done.

---
+++ lib/painting.rb
@@ -0,0 +1,17 @@
+class Painting
+    attr_reader :value, :title
+
+    @@data = []
+    @@artist_lookup = Hash.new{|h,k| h[k] = []}
+
+    def initialize(id, title, artist, value)
+    @id, @title, @artist, @value = id, title, artist, value
+    end
+
+    def self.create(id, title, artist, value)
+    @@data << (new_p = Painting.new(id, title, artist, value))
+    @@artist_lookup[artist] << new_p
+    new_p
+    end
+
+end
